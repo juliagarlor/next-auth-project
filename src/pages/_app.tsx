d31126fa5c2f27e1397a7c04390339fad9f6d343
juliagarlor <julia.garcia.lorente@gmail.com>
2023-08-08 11:42:27 +0200

    First commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { AppProps } from "next/app";
+import "@/styles/globals.css";
+
+export default function App({
+    Component, 
+    pageProps: {
+        //session, 
+        ...pageProps},
+
+}: AppProps){
+
+    return (
+        <Component {...pageProps}/>
+    )
+}
